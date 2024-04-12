da8d31284f7b1a7bc5d6b24f022a77480491bfb7
albertkun <albertk@gmx.com>
2024-04-12 14:03:35 -0700

    refactor: legend closed by default on mobile devices fix: removed window onload legend in `websocket.js`

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 let showMini = false;
 
+// Detect if the screen is a mobile device and start the legend minified
+if(window.innerWidth <= 600) {
+    showMini = true;
+}
+
 document.getElementById('legend-close').addEventListener('click', () => {
     showMini = true;
     adjustMiniDisplay();
@@ -25,9 +30,11 @@ window.addEventListener('resize', () => {
     var h = document.documentElement.clientHeight;
 
     if (w > 600) {
-        document.getElementById('legend').style.display = 'block';
-        document.getElementById('legend-mini').style.display = 'none';
+        showMini = false;
+        adjustMiniDisplay();
     } else {
+        showMini = true;
         adjustMiniDisplay();
     }
 });
+
